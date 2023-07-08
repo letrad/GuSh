9d1cb9fe78893f6e91d3f5a63a50a60e738a08b1
Letrad <letrad@disroot.org>
2023-07-08 15:13:07 +1000

    Formatted

--- main.cpp
+++ main.cpp
@@ -25,11 +25,12 @@
 #include <utility>
 #include <mutex>
 #include <unistd.h>
-const char* getHomeDirectory() {
+
+const char *getHomeDirectory() {
     return getpwuid(getuid())->pw_dir;
 }
 
-std::string shortenPath(const std::string& path) {
+std::string shortenPath(const std::string &path) {
     std::string shortenedPath = path;
     const char *homedir = getHomeDirectory();
 
@@ -40,7 +41,7 @@ std::string shortenPath(const std::string& path) {
     return shortenedPath;
 }
 
-void handleToken(const std::string& token, std::vector<std::string>& tokens) {
+void handleToken(const std::string &token, std::vector<std::string> &tokens) {
     if (!token.empty()) {
         if (token[0] == '$') {
             auto it = Commands::envVariables.find(token.substr(1));
@@ -55,12 +56,12 @@ void handleToken(const std::string& token, std::vector<std::string>& tokens) {
     }
 }
 
-std::string exSubstitute(const std::string& command) {
+std::string exSubstitute(const std::string &command) {
     char buffer[128];
     std::string result = "";
 
     // Open pipe to file
-    FILE* pipe = popen(command.c_str(), "r");
+    FILE *pipe = popen(command.c_str(), "r");
     if (!pipe) {
         return "popen failed!";
     }
@@ -75,89 +76,89 @@ std::string exSubstitute(const std::string& command) {
     return result;
 }
 
-std::vector<std::vector<std::string>> splitCommand(const std::string& command) {
+std::vector<std::vector<std::string>> splitCommand(const std::string &command) {
     std::vector<std::vector<std::string>> commands;
-    std::vector<std::string> tokens,expanded;
+    std::vector<std::string> tokens, expanded;
     std::string token; //See Lexer.cpp
-    size_t idx=0;
+    size_t idx = 0;
 
     std::string commandWithAliasesReplaced = command;
-    for (const auto& alias : Commands::aliasMap) {
+    for (const auto &alias: Commands::aliasMap) {
         std::string pattern = "(\\b)" + alias.first + "(\\b)";  // \b represents a word boundary
         std::regex r(pattern);
         commandWithAliasesReplaced = std::regex_replace(commandWithAliasesReplaced, r, alias.second);
     }
-    
+
     try {
-		while(idx<command.size()) {
-			token=Lexer::LexItem(command,idx);
-			expanded=Lexer::ExpandToken(token);
-			for(auto token :expanded) {
-				if(token=="|") {
-					commands.push_back(tokens);
-					tokens.clear();
-				} else {
-					handleToken(token,tokens);
-				}
-			}
-		}
-	} catch(Lexer::CLexerExcept lexx) {
-	   std::cerr<<lexx.message<<std::endl;
-	   commands.clear();
-	   return commands;
-	}
-    
+        while (idx < command.size()) {
+            token = Lexer::LexItem(command, idx);
+            expanded = Lexer::ExpandToken(token);
+            for (auto token: expanded) {
+                if (token == "|") {
+                    commands.push_back(tokens);
+                    tokens.clear();
+                } else {
+                    handleToken(token, tokens);
+                }
+            }
+        }
+    } catch (Lexer::CLexerExcept lexx) {
+        std::cerr << lexx.message << std::endl;
+        commands.clear();
+        return commands;
+    }
+
     commands.push_back(tokens);
 
     return commands;
 }
 
-static int exWithEnv(const char *command,const std::vector<std::string>& cmds) {	
-	std::vector<char*> cstyle_commands;
-	for (const auto& cmd : cmds) {
-		cstyle_commands.push_back(const_cast<char*>(cmd.c_str()));
-	}
-	cstyle_commands.push_back(nullptr);
-	
-	//Make enviroment array
-	std::vector<char *> cstyle_env;
-	//We will need to allocate the tuples in the format "VAR=value"
-	std::vector<std::string> alloced_var_tuples;
-	#if defined (__linux__)
-	//Here's the deal const char **environ; contains "var=val" stuff,I will make a "clone"
-	//of existing ones,then I will add our stuff from Commands::envVariables
-	//man 7 environ
-	extern char **environ;
-	for(size_t i=0;environ[i];i++)
-		cstyle_env.push_back(environ[i]);
-	for(const auto& it: Commands::envVariables) {
-		std::string dummy=it.first;
-		dummy+="=";
-		dummy+=it.second;
-		alloced_var_tuples.push_back(dummy);
-		cstyle_env.push_back(const_cast<char*>(dummy.c_str()));
-	}
-	cstyle_env.push_back(nullptr);
+static int exWithEnv(const char *command, const std::vector<std::string> &cmds) {
+    std::vector<char *> cstyle_commands;
+    for (const auto &cmd: cmds) {
+        cstyle_commands.push_back(const_cast<char *>(cmd.c_str()));
+    }
+    cstyle_commands.push_back(nullptr);
 
-	return execvpe(command, cstyle_commands.data(),cstyle_env.data());
-	#elif defined (__FreeBSD__)
-	for(const auto& it: Commands::envVariables) {
-		const char *exist;
-		if((exist=getenv(it.first.c_str()))) {
-			if(strcmp(exist,it.second.c_str()))
-				goto set;
-		} else {
+    //Make enviroment array
+    std::vector<char *> cstyle_env;
+    //We will need to allocate the tuples in the format "VAR=value"
+    std::vector<std::string> alloced_var_tuples;
+#if defined (__linux__)
+    //Here's the deal const char **environ; contains "var=val" stuff,I will make a "clone"
+    //of existing ones,then I will add our stuff from Commands::envVariables
+    //man 7 environ
+    extern char **environ;
+    for (size_t i = 0; environ[i]; i++)
+        cstyle_env.push_back(environ[i]);
+    for (const auto &it: Commands::envVariables) {
+        std::string dummy = it.first;
+        dummy += "=";
+        dummy += it.second;
+        alloced_var_tuples.push_back(dummy);
+        cstyle_env.push_back(const_cast<char *>(dummy.c_str()));
+    }
+    cstyle_env.push_back(nullptr);
+
+    return execvpe(command, cstyle_commands.data(), cstyle_env.data());
+#elif defined (__FreeBSD__)
+    for(const auto& it: Commands::envVariables) {
+        const char *exist;
+        if((exist=getenv(it.first.c_str()))) {
+            if(strcmp(exist,it.second.c_str()))
+                goto set;
+        } else {
 set:
-			setenv(it.first.c_str(),it.second.c_str(),1);
-		}
-	}
-	return execvp(command, cstyle_commands.data());
-	#else
-	#error "Platform not supported"
-	#endif
-}  
+            setenv(it.first.c_str(),it.second.c_str(),1);
+        }
+    }
+    return execvp(command, cstyle_commands.data());
+#else
+#error "Platform not supported"
+#endif
+}
 
-void exExternal(const std::vector<std::string>& commands) {
+void exExternal(const std::vector<std::string> &commands) {
     if (commands.empty()) {
         std::cerr << "GuSH: No command provided\n";
         return;
@@ -190,8 +191,8 @@ void exExternal(const std::vector<std::string>& commands) {
     } else if (pid == 0) {  // Child process
         signal(SIGINT, SIG_DFL);  // Reset SIGINT handler
 
-		exWithEnv(const_cast<const char*>(commands[0].c_str()),finalCommands);
-        
+        exWithEnv(const_cast<const char *>(commands[0].c_str()), finalCommands);
+
         // If execvp returns, an error occurred.
         std::cerr << "GuSH: The specified command could not be found: " << finalCommands[0] << "\n";
         exit(EXIT_FAILURE);  // End the child process.
@@ -208,7 +209,7 @@ void exExternal(const std::vector<std::string>& commands) {
 }
 
 
-void exPipedCommands(std::vector<std::vector<std::string>>& commands) {
+void exPipedCommands(std::vector<std::vector<std::string>> &commands) {
     int pipefds[2 * commands.size() - 2];
     for (int i = 0; i < commands.size() - 1; ++i) {
         if (pipe(pipefds + i * 2) < 0) {
@@ -242,7 +243,7 @@ void exPipedCommands(std::vector<std::vector<std::string>>& commands) {
             for (int j = 0; j < 2 * commands.size() - 2; ++j)
                 close(pipefds[j]);
 
-            if (exWithEnv(const_cast<const char*>(commands[i][0].c_str()),commands[i]) < 0) {
+            if (exWithEnv(const_cast<const char *>(commands[i][0].c_str()), commands[i]) < 0) {
                 perror(commands[i][0].c_str());
                 exit(EXIT_FAILURE);
             }
@@ -257,23 +258,23 @@ void exPipedCommands(std::vector<std::vector<std::string>>& commands) {
 }
 
 
-std::vector<std::string> getCommandMatches(const char* text) {
+std::vector<std::string> getCommandMatches(const char *text) {
     std::vector<std::string> matches;
-    if(!strlen(text)) return matches;
+    if (!strlen(text)) return matches;
     std::string pathEnv = std::getenv("PATH");
     std::istringstream pathStream(pathEnv);
     std::string path;
     while (getline(pathStream, path, ':')) {
         try {
-			if(std::filesystem::exists(path))
-				for (const auto& entry : std::filesystem::directory_iterator(path)) {
-					//Here's the deal,a user may have a invalid path in their directory,do ignore it 
-					const std::string& filename = entry.path().filename().string();
-					if (filename.find(text) == 0) {
-						matches.push_back(filename);
-					}
-				}
-        } catch (const std::filesystem::filesystem_error& e) {
+            if (std::filesystem::exists(path))
+                for (const auto &entry: std::filesystem::directory_iterator(path)) {
+                    //Here's the deal,a user may have a invalid path in their directory,do ignore it
+                    const std::string &filename = entry.path().filename().string();
+                    if (filename.find(text) == 0) {
+                        matches.push_back(filename);
+                    }
+                }
+        } catch (const std::filesystem::filesystem_error &e) {
             std::cerr << "GuSH: Filesystem error: " << e.what() << std::endl;
         }
     }
@@ -281,63 +282,63 @@ std::vector<std::string> getCommandMatches(const char* text) {
 }
 
 std::vector<std::string> getDirMatches(const char *path) {
-	size_t cnt,dir_sz;
-	bool expanded_home_dir = false;
-	std::vector<std::string> matches;
-	std::string pat=path;
-	if(!pat.size()) return matches;
-	if(0==pat.find("~/")) {
-		expanded_home_dir = true;
-		dir_sz = std::string(getHomeDirectory()).size();
-		pat.replace(0,1,getHomeDirectory());
-	}
-	if(std::filesystem::is_directory(pat)) {
-		if(pat.back()=='/')
-			pat += "*";
-	} else if(pat.back()!='*')
-		pat += "*";
-	glob_t gl;
+    size_t cnt, dir_sz;
+    bool expanded_home_dir = false;
+    std::vector<std::string> matches;
+    std::string pat = path;
+    if (!pat.size()) return matches;
+    if (0 == pat.find("~/")) {
+        expanded_home_dir = true;
+        dir_sz = std::string(getHomeDirectory()).size();
+        pat.replace(0, 1, getHomeDirectory());
+    }
+    if (std::filesystem::is_directory(pat)) {
+        if (pat.back() == '/')
+            pat += "*";
+    } else if (pat.back() != '*')
+        pat += "*";
+    glob_t gl;
     gl.gl_offs = 0;
     glob(pat.c_str(), GLOB_DOOFFS, NULL, &gl);
-    if(gl.gl_pathv)
-		for(cnt=0;gl.gl_pathv[cnt];cnt++) {
-			matches.push_back(gl.gl_pathv[cnt]);
-			if(expanded_home_dir)
-				matches.back().replace(0,dir_sz,"~");
-		}
-    globfree(&gl);  
+    if (gl.gl_pathv)
+        for (cnt = 0; gl.gl_pathv[cnt]; cnt++) {
+            matches.push_back(gl.gl_pathv[cnt]);
+            if (expanded_home_dir)
+                matches.back().replace(0, dir_sz, "~");
+        }
+    globfree(&gl);
     return matches;
 }
 
-typedef char** (*CompletionFunc)(const char*, int, int);
+typedef char **(*CompletionFunc)(const char *, int, int);
 
 //Force a "C" ABI for libreadline api
 extern "C" {
-char** gushCompletion(const char* text, int start, int end) {
-    if (start != 0&&!strlen(text)) {
+char **gushCompletion(const char *text, int start, int end) {
+    if (start != 0 && !strlen(text)) {
         return nullptr;
     }
 
     rl_attempted_completion_over = 1;
     std::vector<std::string> pmatches = getCommandMatches(text);
     std::vector<std::string> dmatches = getDirMatches(text);
-    pmatches.insert(pmatches.end(),dmatches.begin(),dmatches.end());
+    pmatches.insert(pmatches.end(), dmatches.begin(), dmatches.end());
 
-    std::sort(pmatches.begin(),pmatches.end());
-    
+    std::sort(pmatches.begin(), pmatches.end());
+
     if (pmatches.empty()) {
         return nullptr;
     }
 
-	static std::shared_ptr<std::vector<std::string>> precomputed=nullptr;
-	precomputed=std::make_shared<std::vector<std::string>>(pmatches);
+    static std::shared_ptr<std::vector<std::string>> precomputed = nullptr;
+    precomputed = std::make_shared<std::vector<std::string>>(pmatches);
 
-    char** completionMatches = rl_completion_matches(text, [](const char* text, int state) -> char* {
+    char **completionMatches = rl_completion_matches(text, [](const char *text, int state) -> char * {
         static size_t index = 0;
         if (state == 0) {
             index = 0;
         }
-        
+
         if (index < precomputed->size()) {
             return strdup(precomputed->at(index++).c_str());
         }
@@ -345,23 +346,23 @@ char** gushCompletion(const char* text, int start, int end) {
         return nullptr;
     });
 
-	precomputed.reset();
+    precomputed.reset();
 
     return completionMatches;
 }
 };
 
-std::unordered_map<std::string, std::function<void(const std::vector<std::string>&)>> commandMap = {
-            {"exit", [](const std::vector<std::string>&) {
-                exit(0);
-            }},
-            {"pwd", Commands::cmdPwd},
-            {"cd", Commands::cmdCd},
-            {"export", Commands::cmdExport},
-            {"alias", Commands::cmdAlias}
+std::unordered_map<std::string, std::function<void(const std::vector<std::string> &)>> commandMap = {
+        {"exit",   [](const std::vector<std::string> &) {
+            exit(0);
+        }},
+        {"pwd",    Commands::cmdPwd},
+        {"cd",     Commands::cmdCd},
+        {"export", Commands::cmdExport},
+        {"alias",  Commands::cmdAlias}
 };
 
-std::string exCmd(const std::string& command) {
+std::string exCmd(const std::string &command) {
     std::string modifiedCommand = command;
 
     // New code to handle command substitution
@@ -369,9 +370,9 @@ std::string exCmd(const std::string& command) {
     while (pos != std::string::npos) {
         std::size_t endPos = command.find(")", pos);
         if (endPos != std::string::npos) {
-            std::string innerCommand = command.substr(pos+2, endPos-pos-2);
+            std::string innerCommand = command.substr(pos + 2, endPos - pos - 2);
             std::string substitution = exSubstitute(innerCommand);
-            modifiedCommand.replace(pos, endPos-pos+1, substitution);
+            modifiedCommand.replace(pos, endPos - pos + 1, substitution);
         }
         pos = command.find("$(", pos + 1);
     }
@@ -380,13 +381,13 @@ std::string exCmd(const std::string& command) {
 
     if (commands.size() > 1) {
         exPipedCommands(commands);
-    } else if(commands.size()) {
+    } else if (commands.size()) {
         if (!commands[0].empty()) {
-            const std::string& cmd = commands[0][0];
+            const std::string &cmd = commands[0][0];
             auto it = commandMap.find(cmd);
             if (it != commandMap.end()) {
                 //First item in commands is the command name,so i will remove it as I only want the arguments
-	        commands[0].erase(commands[0].begin());
+                commands[0].erase(commands[0].begin());
                 it->second(commands[0]);
             } else {
                 exExternal(commands[0]);
@@ -427,12 +428,13 @@ int main() {
             }
         }
         exConfig();
-        char* input;
+        char *input;
         while (true) {
             char currentDir[PATH_MAX];
             if (getcwd(currentDir, sizeof(currentDir)) != nullptr) {
                 // Just make it look like robbyrussel while I work on things
-                std::string prompt = "\u001b[1m\u001b[96m" + std::string(shortenPath(currentDir)) + " \u001b[32m➜\033[0m ";
+                std::string prompt =
+                        "\u001b[1m\u001b[96m" + std::string(shortenPath(currentDir)) + " \u001b[32m➜\033[0m ";
                 input = readline(prompt.c_str());
             } else {
                 std::cerr << "Failed to get current directory" << std::endl;
@@ -450,7 +452,7 @@ int main() {
         }
 
         free(input); // In case the program exits normally, free the memory.
-    } catch (const std::exception& e) {
+    } catch (const std::exception &e) {
         std::cerr << "Error: " << e.what() << std::endl;
         return 1;
     }
